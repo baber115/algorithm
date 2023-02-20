4fd13ef9b03aff7f15c5271269ead80bd6b3c14f
范宇波 <934472858@qq.com>
2023-02-20 09:51:29 +0800

    leetcode 算法入门第一天

---
+++ binary_search/搜索插入位置_test.go
@@ -0,0 +1,42 @@
+package binary_search
+
+import (
+	"fmt"
+	"testing"
+)
+
+// https://leetcode.cn/problems/search-insert-position
+
+func TestSearchInsert(t *testing.T) {
+	nums, target := []int{1, 3, 5, 6}, 7
+	res := searchInsertI(nums, target)
+	fmt.Println(res)
+}
+
+func searchInsertI(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] >= target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}
+
+func searchInsertII(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	ans := len(nums)
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] >= target {
+			ans = mid
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return ans
+}
---
+++ binary_search/第一个错误的版本_test.go
@@ -0,0 +1,36 @@
+package binary_search
+
+import "sort"
+
+/**
+ * Forward declaration of isBadVersion API.
+ * @param   version   your guess about first bad version
+ * @return 	 	      true if current version is bad
+ *			          false if current version is good
+ * func isBadVersion(version int) bool;
+ */
+
+// https://leetcode.cn/problems/first-bad-version/
+
+func firstBadVersionII(n int) int {
+	return sort.Search(n, func(i int) bool {
+		return isBadVersion(i)
+	})
+}
+
+func firstBadVersionI(n int) int {
+	left, right := 1, n
+	for left < right {
+		mid := left + (right-left)/2
+		if isBadVersion(mid) {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}
+
+func isBadVersion(version int) bool {
+	return true
+}
